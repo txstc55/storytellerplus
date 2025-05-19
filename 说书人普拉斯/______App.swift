03cb9ac596f8d5b0a7d78c9d6b7fa967d9a942d0
txstc55 <txstc55@gmail.com>
2025-05-18 21:23:20 -0700

    Initial Commit

---
+++ 说书人普拉斯/______App.swift
@@ -0,0 +1,32 @@
+//
+//  ______App.swift
+//  说书人普拉斯
+//
+//  Created by Xuan Tang on 5/18/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ______App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
